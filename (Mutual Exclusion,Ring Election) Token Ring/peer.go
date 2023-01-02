a00ffd1104737b448940685b0761c07613b13a96
soer4769 <soer4769@gmail.com>
2023-01-02 13:28:37 +0100

    added critical section to token ring

--- (Mutual Exclusion,Ring Election) Token Ring/peer.go
+++ (Mutual Exclusion,Ring Election) Token Ring/peer.go
@@ -35,15 +35,17 @@ type client struct {
     coordId     int32
     neighId     int32
     begunElect  bool
+    doneCrit    bool
+    waitCrit    bool
     neighbor    GoMETR.METRClient
 }
 
 // ---------------------------- //
 // ---------- METHODS --------- //
 // ---------------------------- //
-func (c *client) Send(id int32, electstat int32) {
+func (c *client) Send(id int32, electstat int32, critical bool) {
     c.lamport++
-    msg := GoMETR.Token{Id: id, Electstat: electstat, Lamport: c.lamport}
+    msg := GoMETR.Token{Id: id, Electstat: electstat, Lamport: c.lamport, Critical: critical}
     log.Printf("Sendt message '%v' to '%v' (lamport %v)", electstat, c.neighId, c.lamport)
     if _, err := c.neighbor.PassToken(context.Background(), &msg, grpc.WaitForReady(true)); err != nil {
         log.Fatalf("Failed to send %v a message: %v", id, err)
@@ -67,30 +69,45 @@ func (c *client) Ring(token *GoMETR.Token) {
                 if token.Id > c.id {
                     log.Printf("Recieved higher id message %v, forwarding...",token.Id)
                     c.coordId = token.Id
-                    c.Send(token.Id, token.Electstat)
+                    c.Send(token.Id, token.Electstat, false)
                 } else if token.Id < c.id && !c.begunElect {
                     log.Printf("Recieved <id election message %v without starting election, forwarding own...",token.Id)
                     c.begunElect = true
-                    c.Send(c.id, token.Electstat)
+                    c.Send(c.id, token.Electstat, false)
                 } else if token.Id == c.id {
                     log.Println("Recieved own election message...")
-                    c.Send(c.id, ELECTED)
+                    c.Send(c.id, ELECTED, false)
                 }
             case ELECTED:
                 if token.Id != c.id {
                     log.Printf("Recieved elected message %v, now follower", token.Id)
                     c.state = FOLLOWER
-                    c.Send(token.Id, token.Electstat)
+                    c.Send(token.Id, token.Electstat, false)
                 } else if token.Id == c.id {
                     log.Println("Recieved own elected message, now coordinator")
                     c.state = COORDINATOR
-                    c.Send(c.id, -1)
+                    c.Send(c.id, -1, false)
                 }
         }
+    } else if c.state == FOLLOWER && !c.doneCrit && !token.Critical {
+        log.Println("Asking coordinator permission entering critical section...")
+        c.waitCrit = true
+        c.Send(c.id,-1,true)
+    } else if c.state == FOLLOWER && !c.doneCrit && c.waitCrit && token.Critical {
+        c.lamport++
+        log.Println("Inside of Critical section")
+        time.Sleep(5 * time.Second)
+        log.Println("Outside of critical section")
+        c.doneCrit = true
+        c.waitCrit = false
+        c.Send(c.id,-1,false)
+    } else if c.state == COORDINATOR && token.Critical {
+        log.Printf("Follower %v asking access to critical section...",token.Id)
+        c.Send(c.id,-1,true)
     } else {
         log.Printf("Recieved token %v from neighbor, forwading...",token.Electstat)
         time.Sleep(time.Second*2)
-        c.Send(c.id, token.Electstat)
+        c.Send(token.Id, token.Electstat, token.Critical)
     }
 }
 
@@ -142,7 +159,7 @@ func main() {
         if !c.begunElect {
             c.begunElect = true
             log.Println("Begins new election...")
-            c.Send(int32(pid), ELECTION)
+            c.Send(int32(pid), ELECTION, false)
         }
     }
 }
